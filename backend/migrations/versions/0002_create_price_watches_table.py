b4d069055ea5778182530b06fbe440a403385213
mjanid <130252906+mjanid@users.noreply.github.com>
2026-02-10 15:34:42 +0100

    Update 0002_create_price_watches_table.py
    Shortened string

--- backend/migrations/versions/0002_create_price_watches_table.py
+++ backend/migrations/versions/0002_create_price_watches_table.py
@@ -1,7 +1,7 @@
 
 """Add last_alerted_at to price_watches.
 
-Revision ID: 002_price_watches_last_alerted_at
+Revision ID: 002_pw_last_alerted_at
 Revises: 001_initial
 Create Date: 2026-02-09
 """
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision: str = "002_price_watches_last_alerted_at"
+revision: str = "002_pw_last_alerted_at"
 down_revision: Union[str, None] = "001_initial"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
